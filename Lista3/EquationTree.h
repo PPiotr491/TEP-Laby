0a40ea733a2214393ba8b94848eb60cf5902fdb6
PPiotr491 <ppiotrowski491@gmail.com>
2025-11-16 20:43:28 +0100

    Listy3 equationtree.h

--- Lista3/EquationTree.h
+++ Lista3/EquationTree.h
@@ -1,8 +1,35 @@
 #ifndef TEP_EQUATIONTREE_H
 #define TEP_EQUATIONTREE_H
+#include "Node.h"
+#include <string>
 
 
 class EquationTree {
+
+public:
+    EquationTree();
+    EquationTree(std::string equation);
+    EquationTree(const EquationTree& nodeToCopy);
+
+    EquationTree& operator=(const int value);
+    EquationTree& operator+(const EquationTree &other);
+
+    Node getRoot() const {
+        return root;
+    }
+
+    void setRoot(const Node &root) {
+        this->root = root;
+    }
+
+private:
+    Node root;
+
+    std::string enterNewEquation(std::string equation);
+    std::string vars();
+    std::string print();
+    double comp(double* varTable);
+    void join(std::string);
 };
 
 
